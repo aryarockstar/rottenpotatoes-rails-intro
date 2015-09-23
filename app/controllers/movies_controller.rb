75979c27453802fa6160a31440fef2f0cae2281c
Abhinandan Aryadipta <aryarockstar@tamu.edu>
2015-09-23 01:29:27 -0300

    Intermediate Checking

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,19 +14,65 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     if (params[:ratings])
       @selected_ratings = params[:ratings].keys
+    elsif (session[:ratings])
+      @selected_ratings = session[:ratings].keys
     else
-      @selected_ratings = []
+      @selected_ratings = @all_ratings
     end
     val = params[:sort].to_s
-    if (val == "title")
-      @movies = Movie.order(:title)
+    if ((val == "title" ) || (session[:sort]=="title" && val!="release_date"))
+			if (params[:ratings])
+        @movies = Movie.where(:rating => @selected_ratings).order(:title)
+        session[:ratings] = params[:ratings]
+				@selected_ratings = params[:ratings].keys
+				if(session[:sort]=="title" && val!="title")
+					redirect_to movies_path(:sort => session[:sort],:ratings => params[:ratings])
+				end
+			elsif (session[:ratings])
+				@movies = Movie.where(:rating => @selected_ratings).order(:title)
+				if(session[:sort]=="title")
+				redirect_to movies_path(:sort => session[:sort],:ratings => session[:ratings])
+				else
+				redirect_to movies_path(:sort => params[:sort],:ratings => session[:ratings])
+				end
+			else
+      	@movies = Movie.order(:title)
+				if (session[:sort] && val!="title")
+					redirect_to movies_path(:sort => "title")
+				end 
+			end
       @title_header = "hilite"
-    elsif (val == "release_date")
-      @movies = Movie.order(:release_date)
+			session[:sort] = "title"
+    elsif ((val == "release_date" ) || (session[:sort]=="release_date" && val!="title"))
+			if (params[:ratings])
+        @movies = Movie.where(:rating => @selected_ratings).order(:release_date)
+        session[:ratings] = params[:ratings]
+				@selected_ratings = params[:ratings].keys
+				if(session[:sort]=="release_date" && val!="release_date")
+				redirect_to movies_path(:sort => session[:sort],:ratings => params[:ratings])
+				end
+			elsif (session[:ratings])
+				@movies = Movie.where(:rating => @selected_ratings).order(:release_date)
+				if(session[:sort]=="release_date")
+				redirect_to movies_path(:sort => session[:sort],:ratings => session[:ratings])
+				else
+				redirect_to movies_path(:sort => params[:sort],:ratings => session[:ratings])
+				end
+			else
+      	@movies = Movie.order(:release_date)
+				if (session[:sort] && val!="release_date")
+					redirect_to movies_path(:sort => "release_date")
+				end 
+			end
       @release_date_header = "hilite"
+			session[:sort] = "release_date"
     else
       if (params[:ratings])
         @movies = Movie.where(:rating => @selected_ratings)
+        session[:ratings] = params[:ratings]
+      elsif (session[:ratings])
+        @movies = Movie.where(:rating => @selected_ratings)
+				redirect_to movies_path(:ratings => session[:ratings])
       else
         @movies = Movie.all
         @selected_ratings = Movie.all_ratings
